f4aa11a044f489dda09131cd1fed5bb64460a49a
fabiano027 <165963962+fabiano027@users.noreply.github.com>
2024-04-03 19:29:58 -0300

    Add files via upload

---
+++ Atividade.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(){
+
+
+float base, altura, area;
+
+
+printf  ("\nDigite o numero da base:");
+scanf ("%f", &base);
+
+printf ("\nDigite o numero da altura:");
+scanf ("%f", &altura);
+ 
+ 	area = base+altura;
+  
+  printf("Total da soma %f\n", area);
+  
+printf ("     # \n");
+printf ("    ## \n");
+printf ("   ### \n");
+
+
+system("pause");
+
+	return 0;
+	
+
+}
